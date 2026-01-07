2cc6bef5ceb1a245b29800a8cdb4ba9c3f13980d
wengyaoyao <zeyaoweng@gmail.com>
2026-01-07 16:40:21 +0800

    Update OISST_calculate_mhw_north_australia.m

--- code/OISST_calculate_mhw_north_australia.m
+++ code/OISST_calculate_mhw_north_australia.m
@@ -1,6 +1,9 @@
 %This is using OISST data to calculate MHW
 % Edit：2026.1.3
 %==========================================================================
+% Edit:2026.1.7
+% change the climatology period. nwe code add in line 15 and line 21
+%==========================================================================
 clc;clear;close all;
 
 %data input
@@ -8,9 +11,11 @@ load('../output/OISST_t_interp_0_25_north_australia.mat','t','lon','lat','time')
 
 %% calculate MHW
 % [MHW,mclim,m90,mhw_ts]=detect(t,datenum(1982,1,1):datenum(2020,12,31),datenum(1993,1,1),datenum(2012,12,31),datenum(2016,1,1),datenum(2017,12,30));
-[MHW,mclim,m90,mhw_ts]=detect(t,datenum(1982,1,1):datenum(2020,12,31),datenum(1993,1,1),datenum(2012,12,31),datenum(2015,11,1),datenum(2016,4,31));
+%[MHW,mclim,m90,mhw_ts]=detect(t,datenum(1982,1,1):datenum(2020,12,31),datenum(1993,1,1),datenum(2012,12,31),datenum(2015,11,1),datenum(2016,4,30));
+[MHW,mclim,m90,mhw_ts]=detect(t,datenum(1982,1,1):datenum(2020,12,31),datenum(1982,1,1),datenum(2015,12,31),datenum(2015,11,1),datenum(2016,4,30));
 
 %% save data
 
 % save('../output/OISST_MHW_20160101_20171230.mat','MHW','m90','mclim','mhw_ts','lon','lat');
-save('../output/OISST_MHW_20151101_20160431.mat','MHW','m90','mclim','mhw_ts','lon','lat');
+%save('../output/OISST_MHW_20151101_20160431.mat','MHW','m90','mclim','mhw_ts','lon','lat');
+save('../output/OISST_MHW_20151101_20160430_clim_change.mat','MHW','m90','mclim','mhw_ts','lon','lat');
